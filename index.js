ff016a50bf895c78bea571ff07bff9a033af2bf6
Bruno Silva <brunogs.sp@gmail.com>
2018-08-04 21:27:43 -0300

    add: simple server with graphql-yoga

---
+++ index.js
@@ -0,0 +1,19 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+const typeDefs = `
+    type Query {
+        gnar: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        gnar: () => 'gnarly!!!'
+    }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start(
+    () => console.log('graph service running - http://localhost:4000')
+);
